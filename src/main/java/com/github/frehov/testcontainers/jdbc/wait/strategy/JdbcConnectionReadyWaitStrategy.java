3a54074543adb813e4da0adb2465bee79d3fe8fd
Fredrik Hov <37329363+frehov@users.noreply.github.com>
2023-01-03 14:19:18 +0100

    Throw IllegalArgumentException immediately if waitStrategyTarget is not supported for downcasting.

--- src/main/java/com/github/frehov/testcontainers/jdbc/wait/strategy/JdbcConnectionReadyWaitStrategy.java
+++ src/main/java/com/github/frehov/testcontainers/jdbc/wait/strategy/JdbcConnectionReadyWaitStrategy.java
@@ -1,15 +1,19 @@
 package com.github.frehov.testcontainers.jdbc.wait.strategy;
 
 import org.rnorth.ducttape.unreliables.Unreliables;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.ContainerLaunchException;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.wait.strategy.AbstractWaitStrategy;
-import org.testcontainers.delegate.DatabaseDelegate;
 import org.testcontainers.jdbc.JdbcDatabaseDelegate;
 
 import java.util.concurrent.TimeUnit;
 
 public class JdbcConnectionReadyWaitStrategy extends AbstractWaitStrategy {
+
+    private final static Logger logger = LoggerFactory.getLogger(JdbcConnectionReadyWaitStrategy.class);
+
     private static final String DEFAULT_VALIDATION_QUERY = "select 1";
     private static final String TIMEOUT_ERROR = "Timed out waiting for SQL database to be accessible for query execution";
 
@@ -19,22 +23,32 @@ public class JdbcConnectionReadyWaitStrategy extends AbstractWaitStrategy {
     private String validationQuery = DEFAULT_VALIDATION_QUERY;
 
     public JdbcConnectionReadyWaitStrategy() {}
+
     public JdbcConnectionReadyWaitStrategy(String validationQuery) {
         this.validationQuery = validationQuery;
     }
 
     @Override
     protected void waitUntilReady() {
-        Runnable testDatabaseQuery = () -> {
-            try (DatabaseDelegate databaseDelegate = this.getDatabaseDelegate()) {
-                databaseDelegate.execute(validationQuery, "", 1, false, false);
+        if (!isWaitStrategyTargetSupported()) {
+            logger.trace("Unable to downcast \"{}\" to \"{}\"", this.waitStrategyTarget.getClass().getName(), CONTAINER_CLASS.getName());
+            throw new IllegalArgumentException(String.format(
+                    "Container [%s] was not castable to type: %s", this.waitStrategyTarget.getClass().getName(), CONTAINER_CLASS.getName())
+            );
+        }
+        JdbcDatabaseContainer<?> container = CONTAINER_CLASS.cast(this.waitStrategyTarget);
+        logger.debug("Executing connection validation-query \"{}\" on database \"{}\"", validationQuery, container.getJdbcUrl());
+
+        Runnable runValidationQuery = () -> {
+            try (JdbcDatabaseDelegate delegate = new JdbcDatabaseDelegate(container, "")) {
+                delegate.execute(validationQuery, "", 1, false, false);
             }
         };
 
         // execute validation query until success or timeout is reached
         try {
             Unreliables.retryUntilSuccess((int) this.startupTimeout.getSeconds(), TimeUnit.SECONDS, () -> {
-                this.getRateLimiter().doWhenReady(testDatabaseQuery);
+                this.getRateLimiter().doWhenReady(runValidationQuery);
                 return true;
             });
         } catch (Exception e) {
@@ -42,10 +56,7 @@ public class JdbcConnectionReadyWaitStrategy extends AbstractWaitStrategy {
         }
     }
 
-    private DatabaseDelegate getDatabaseDelegate() {
-        if (CONTAINER_CLASS.isInstance(this.waitStrategyTarget)) {
-            return new JdbcDatabaseDelegate(CONTAINER_CLASS.cast(this.waitStrategyTarget), "");
-        }
-        throw new IllegalArgumentException("Container was not of supported type " + CONTAINER_CLASS.getSimpleName());
+    private boolean isWaitStrategyTargetSupported() {
+        return CONTAINER_CLASS.isInstance(this.waitStrategyTarget);
     }
 }
